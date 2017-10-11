ff1a7de1e8e4671ef52b66af1022f78c3b18e1ab
Stas <frolkin-stas@yandex.ru>
2017-10-11 17:23:39 +0500

    worked ble, uart modules

---
+++ ble_uart_dfu_zip.bat
@@ -0,0 +1,1 @@
+nrfutil pkg generate --hw-version 52 --sd-req 0x9D --application-version 4 --application f:\Projects\nrf52832\nRF5_SDK_14.0.0_3bcc1f7\examples\ble_peripheral\ble_uart_dfu\pca10040\s132\arm5_no_packs\_build\nrf52832_xxaa.hex --key-file f:\Projects\nrf52832\private.pem ble_uart_dfu.zip
